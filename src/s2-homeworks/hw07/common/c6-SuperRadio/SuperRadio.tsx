7c421b36dbe6dbbf3a0601b886b55eb2bf8d6c1b
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-08-02 22:18:25 +0300

    hW7

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -52,7 +52,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
-                      name={o.value}
+                      name={'Junior'}
                       checked={o.id===value}
                       value={o.id}
                       // name, checked, value делают студенты
