f7996fab171f14970ac7a0c8b4ab77439726d4b1
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-09-16 00:08:04 +0300

    HW9 Clock and date

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -41,7 +41,7 @@ const onMouseLeave = () => { // пишут студенты // спрятать 
 }
 
 const stringTime =`${date.getHours().toString().padStart(2,'0')}:${date.getMinutes().toString().padStart(2,'0')}:${date.getSeconds().toString().padStart(2,'0')}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-const stringDate = `${date.getDay().toString().padStart(2,'0')}.${date.getMonth().toString().padStart(2,'0')}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+const stringDate = `${date.getDay().toString().padStart(2,'0')}.${(date.getMonth()+1).toString().padStart(2,'0')}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     const daysOfWeek = [
         'Sunday',
